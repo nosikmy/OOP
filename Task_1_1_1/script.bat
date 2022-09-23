1e812442f9c45611f36984d2aa9978a92c9924cc
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-09-24 01:26:13 +0700

    Update script.bat

--- Task_1_1_1/script.bat
+++ Task_1_1_1/script.bat
@@ -1,6 +1,6 @@
 javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.aramazanova
 
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\ablaginin\Heapsort.java
+javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\aramazanova\Main.java
 
 mkdir .\make\jar
-jar cf .\make\jar\Heapsort.jar -C .\make\bin .
+jar cf .\make\jar\Main.jar -C .\make\bin .
