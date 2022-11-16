ff2b17338dc0949ff0a664e44bf9f0a60a8fb8c8
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-11-17 05:50:04 +0700

    reviewdog

--- Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
@@ -1,13 +1,13 @@
 package ru.nsu.aramazanova1;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
+import org.junit.jupiter.api.Assertions;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Tests for the function for finding entries of a substring in a string.
@@ -18,7 +18,8 @@ public class SubstringTest {
         List<Integer[]> ans = new ArrayList<>();
         List<Integer[]> actual = new ArrayList<>();
         ans.add(new Integer[]{0, 7});
-        try (BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/example1.txt"), UTF_8))) {
+        try (BufferedReader file = new BufferedReader(new InputStreamReader(
+                new FileInputStream("src/test/resources/example1.txt"), UTF_8))) {
             actual = Substring.searchSubstring(file, "пирог");
         }
         Assertions.assertEquals(ans.size(), actual.size());
@@ -32,7 +33,8 @@ public class SubstringTest {
     public void exampleTest2() throws IOException {
         List<Integer[]> ans = new ArrayList<>();
         List<Integer[]> actual = new ArrayList<>();
-        try (BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/example2.txt"), UTF_8))) {
+        try (BufferedReader file = new BufferedReader(new InputStreamReader(
+                new FileInputStream("src/test/resources/example2.txt"), UTF_8))) {
             actual = Substring.searchSubstring(file, "пирог");
         }
         Assertions.assertEquals(ans.size(), actual.size());
@@ -50,7 +52,8 @@ public class SubstringTest {
         ans.add(new Integer[]{0, 0});
         ans.add(new Integer[]{0, 2});
         ans.add(new Integer[]{1, 4});
-        try (BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/2.txt"), UTF_8))) {
+        try (BufferedReader file = new BufferedReader(new InputStreamReader(
+                new FileInputStream("src/test/resources/2.txt"), UTF_8))) {
             actual = Substring.searchSubstring(file, "баба");
         }
         Assertions.assertEquals(ans.size(), actual.size());
@@ -76,7 +79,8 @@ public class SubstringTest {
         ans.add(new Integer[]{44204, 5});
         ans.add(new Integer[]{44205, 13});
         ans.add(new Integer[]{63558, 19});
-        try (BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/WarAndPeace.txt"), UTF_8))) {
+        try (BufferedReader file = new BufferedReader(new InputStreamReader(
+                new FileInputStream("src/test/resources/WarAndPeace.txt"), UTF_8))) {
             actual = Substring.searchSubstring(file, "the oak");
         }
         Assertions.assertEquals(ans.size(), actual.size());
@@ -90,7 +94,8 @@ public class SubstringTest {
     public void emptyFileTest() throws IOException {
         List<Integer[]> ans = new ArrayList<>();
         List<Integer[]> actual = new ArrayList<>();
-        try (BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/empty.txt"), UTF_8))) {
+        try (BufferedReader file = new BufferedReader(new InputStreamReader(
+                new FileInputStream("src/test/resources/empty.txt"), UTF_8))) {
             actual = Substring.searchSubstring(file, "");
         }
         Assertions.assertEquals(ans.size(), actual.size());
@@ -112,7 +117,8 @@ public class SubstringTest {
         ans.add(new Integer[]{6789, 987});
         ans.add(new Integer[]{9999, 0});
         CreatorBigFile.createBigFile("src/test/resources/big.txt");
-        try (BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/big.txt"), UTF_8))) {
+        try (BufferedReader file = new BufferedReader(new InputStreamReader(
+                new FileInputStream("src/test/resources/big.txt"), UTF_8))) {
             actual = Substring.searchSubstring(file, "abc");
         }
         Assertions.assertEquals(ans.size(), actual.size());
