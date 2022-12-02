ff4fb433603d58d5959703defd64ec62824c055a
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-12-02 23:40:25 +0700

    rviewdog

--- Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
