73309ed7cd9210eba44164934a442a46e37997cd
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-12-02 23:27:37 +0700

    changes made

---
+++ Task_1_3_1/src/main/java/ru/nsu/aramazanova1/Pair.java
@@ -0,0 +1,4 @@
+package ru.nsu.aramazanova1;
+
+public record Pair(Integer line, Integer entryIndex) {
+}
--- Task_1_3_1/src/main/java/ru/nsu/aramazanova1/Substring.java
+++ Task_1_3_1/src/main/java/ru/nsu/aramazanova1/Substring.java
@@ -19,11 +19,11 @@ public class Substring {
      * @return List of pairs of line and entry index
      * @throws IOException exception in case there are problems with the file
      */
-    public static List<Integer[]> searchSubstring(BufferedReader file, String inputSubstring)
+    public static List<Pair> searchSubstring(BufferedReader file, String inputSubstring)
             throws IOException {
         byte[] bytesOfString = inputSubstring.getBytes();
         String substring = new String(bytesOfString, StandardCharsets.UTF_8);
-        List<Integer[]> answer = new ArrayList<>(new ArrayList<>());
+        List<Pair> answer = new ArrayList<>(new ArrayList<>());
         int c;
         int lengthSubstring = substring.length();
         int globalIdx = 0;
@@ -46,7 +46,7 @@ public class Substring {
 
                 }
                 if (flag == 1) {
-                    Integer[] insert = new Integer[]{line, globalIdx - lengthSubstring + 1};
+                    Pair insert = new Pair(line, globalIdx - lengthSubstring + 1);
                     answer.add(insert);
 
                 }
