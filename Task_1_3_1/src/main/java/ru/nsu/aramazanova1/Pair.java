8638fe0886d9ef47af49e865960d25c37a871489
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-12-02 23:37:44 +0700

    rviewdog

--- Task_1_3_1/src/main/java/ru/nsu/aramazanova1/Pair.java
+++ Task_1_3_1/src/main/java/ru/nsu/aramazanova1/Pair.java
@@ -1,4 +1,10 @@
 package ru.nsu.aramazanova1;
 
+/**
+ * pair of line and entry index.
+ *
+ * @param line line with substring
+ * @param entryIndex index of the first substring character in the line
+ */
 public record Pair(Integer line, Integer entryIndex) {
 }
--- Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
@@ -7,7 +7,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
