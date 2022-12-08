a3856ffd027959e0d6cd9c55ae9669254853d1f1
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-12-08 16:34:13 +0700

    first commit

---
+++ Task_1_5_1/src/test/java/ru/nsu/aramazanova1/CalculatorTest.java
@@ -0,0 +1,37 @@
+package ru.nsu.aramazanova1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Testing calculator.
+ */
+public class CalculatorTest {
+    @Test
+    public void exampleTest(){
+        Assertions.assertEquals(Calculator.calculate(
+                "sin + - 1 2 1"), 0.0);
+    }
+
+    @Test
+    public void test(){
+        Assertions.assertEquals(Calculator.calculate(
+                "+ log cos - 1 1 134"), 134.0);
+    }
+
+    @Test
+    public void exceptionTest(){
+        IllegalArgumentException e =
+                assertThrows(IllegalArgumentException.class,
+                        () -> Calculator.calculate("+ 1"));
+        Assertions.assertEquals("Недостаточно операндов", e.getMessage());
+        e = assertThrows(IllegalArgumentException.class,
+                        () -> Calculator.calculate("logarithm 1"));
+        Assertions.assertEquals("Такого оператора не существует", e.getMessage());
+        e = assertThrows(IllegalArgumentException.class,
+                () -> Calculator.calculate("sin 0 1"));
+        Assertions.assertEquals("Лишний операнд", e.getMessage());
+    }
+}
