fb713246cfa610b3fbf42276e973a02397613175
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-12-18 22:12:29 +0700

    getResourceAsStream

--- Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/aramazanova1/SubstringTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
