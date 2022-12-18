335b70b3d5319ef41a45c23073dce8d8197b8100
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-12-18 23:28:25 +0700

    reviewdog

--- Task_1_1_1/script.bat
+++
@@ -1,8 +0,0 @@
-javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.aramazanova
-
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\aramazanova\HeapSort.java
-
-mkdir .\make\jar
-jar cf .\make\jar\HeapSort.jar -C .\make\bin .
-
-java -classpath .\make\jar\HeapSort.jar ru.nsu.aramazanova.HeapSort
