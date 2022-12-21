d33291c720014f36541a0614b1e657abc2bfd6a3
nosikmy <91544344+nosikmy@users.noreply.github.com>
2022-12-21 23:32:54 +0700

    build.gradle

--- Task_1_5_2/src/main/java/ru/nsu/aramazanova1/NoteBook.java
+++ Task_1_5_2/src/main/java/ru/nsu/aramazanova1/NoteBook.java
@@ -11,26 +11,26 @@ public class NoteBook {
     public NoteBook() {
     }
 
-    public void addNote(String textNote){
+    public void addNote(String textNote) {
         Note newNote = new Note(textNote);
         notebook.add(newNote);
     }
 
-    public void removeNote(String textNote){
+    public void removeNote(String textNote) {
         List<Note> a = notebook.stream().filter(x -> x.getText().equals(textNote)).toList();
         for (Note note : a) {
             notebook.remove(note);
         }
     }
 
-    public void showNotes(){
+    public void showNotes() {
         notebook.sort(Comparator.comparing(Note::getDate));
         notebook.forEach(x -> System.out.println(x.getDate() + " " + x.getText()));
     }
 
-    public void showNotes(String firstDate, String lastDate, List<String> keyWords){
+    public void showNotes(String firstDate, String lastDate, List<String> keyWords) {
         List<Note> showing = new ArrayList<>();
-        for(String keyWord : keyWords){
+        for (String keyWord : keyWords) {
             notebook.stream().filter(x -> x.getText().contains(keyWord)).forEach(showing::add);
         }
         showing.sort(Comparator.comparing(Note::getDate));
