3f015f75f9b981a18a40705a27a33ee8b90f9d42
nosikmy <nosikmy@users.noreply.github.com>
2023-02-13 06:37:44 +0000

    Deploying to gh-pages from @ nosikmy/OOP@5d6fa6b3995a2465ea4ea9a00769be3c50ae3772 🚀

---
+++ Task_1_5_1extra/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.aramazanova1","l":"Calculator"},{"p":"ru.nsu.aramazanova1","l":"Complex"},{"p":"ru.nsu.aramazanova1","l":"Main"}];updateSearchResults();
